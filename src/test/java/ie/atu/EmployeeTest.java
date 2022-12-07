a3935830cf40985f6c4db06293cbb4564e1b4b23
SeanATU <g00292129@atu.ie>
2022-12-07 10:08:31 +0000

    Adding the rest of the test programs

--- src/test/java/ie/atu/EmployeeTest.java
+++ src/test/java/ie/atu/EmployeeTest.java
@@ -1,5 +1,7 @@
 package ie.atu;
 
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
@@ -10,6 +12,53 @@ class EmployeeTest {
         myEmployee = new Employee();
     }
 
+    @Test
+    public void testName(){
+        for(int x =3; x<25; x++){
+            assertEquals(x,myEmployee.getName().length());
+        }
+    }
+
+    @Test
+    public void testPPS_ID(){
+        for(int x =0; x<11; x++){
+            assertEquals(x,myEmployee.getPPS_ID().length());
+        }
+    }
+
+    @Test
+    public void testEmploymentTypeFullTime(){
+        assertEquals("Full-time",myEmployee.getName());
+    }
+    public void testEmploymentTypePartTime(){
+        assertEquals("Part-time",myEmployee.getName());
+    }
+    public void testEmploymentTypeContract(){
+        assertEquals("Contract",myEmployee.getName());
+    }
+
+    @Test
+    public void testGenderMan(){
+        assertEquals("Man",myEmployee.getName());
+    }
+    @Test
+    public void testGenderWoman(){
+        assertEquals("Woman",myEmployee.getName());
+    }
+    @Test
+    public void testGenderNon_Binary(){
+        assertEquals("Non-Binary",myEmployee.getName());
+    }
+
+    @Test
+    public void testAge(){
+        if(myEmployee.getAge()<16){
+            System.out.println("Age must be over 16");
+        }
+        else if (myEmployee.getAge()>66){
+            System.out.println("Age must be under 66");
+        }
+    }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
